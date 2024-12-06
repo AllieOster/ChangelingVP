1b523c191f52435fbc9cc8bd179e55ebe9e226af
allie <allie0ster@gmail.com>
2024-12-06 11:57:57 +0100

    preparing drag & drop lvl2

---
+++ Assets/#Project/Scripts/InGame/DragAndDrop.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InventorySlots : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    private Vector3 initialPosition; 
+
+    public void Start()
+    {
+        RectTransform rt = GetComponent<RectTransform>();
+        initialPosition = rt.position; 
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Begin Drag");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    { 
+        RectTransform rt = GetComponent<RectTransform>();
+
+        Vector3 worldPosition;
+        if (RectTransformUtility.ScreenPointToWorldPointInRectangle((RectTransform)transform.parent, eventData.position, eventData.pressEventCamera, out worldPosition))
+        {
+            rt.position = worldPosition; 
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("End Drag");
+        RectTransform rt = GetComponent<RectTransform>();
+            rt.position = initialPosition; 
+            Debug.Log("Returned to initial position");
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Pointer Enter");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Pointer Exit");
+    }
+}
